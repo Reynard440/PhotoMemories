624893dbd7f29440b4181d6b7fe6e66d02dad403
Reynard Engels <reynardengels@gmail.com>
2021-10-15 07:48:02 +0200

    Official Initial commit: ========================
    Application layering has been structured and all modules reference other relevant modules via config files.
    All pom files have the necessary dependencies.

---
+++ PhotoMemoriesWebSpringBoot/src/main/java/com/photomemories/web/sb/PhotoMemoriesApplication.java
@@ -0,0 +1,11 @@
+package com.photomemories.web.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoMemoriesApplication {
+    public static void main(String[] args){
+        SpringApplication.run(PhotoMemoriesApplication.class, args);
+    }
+}
