8623d1cac232c0c29ecadbdef8fba34f2629a85e
Reynard Engels <reynardengels@gmail.com>
2021-10-24 21:57:34 +0200

    Aws Major commit: ========================
    Can now upload images to the s3 bucket!

--- PhotoMemoriesAws/src/main/java/com/photomemories/aws/config/AwsConfig.java
+++ PhotoMemoriesAws/src/main/java/com/photomemories/aws/config/AwsConfig.java
@@ -3,6 +3,7 @@ package com.photomemories.aws.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.sun.org.slf4j.internal.Logger;
@@ -16,7 +17,8 @@ public class AwsConfig {
 
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials("", "");
-        return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
+        AWSCredentials awsCredentials = new BasicAWSCredentials("AKIAWFFGT4GVHIR6YE7N", "9V9HNDWJDFEorOGirwvz0zGSTrhdpF4cb2JgtOyG");
+
+        return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).withRegion(Regions.US_EAST_1).build();
     }
 }
