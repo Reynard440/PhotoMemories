9849a093e6b85632cd143c1a8b52f6bf114c6d3f
Reynard Engels <reynardengels@gmail.com>
2021-10-27 15:55:04 +0200

    Aws Major commit: ========================
    Removed some properties.

--- PhotoMemoriesAws/src/main/java/com/photomemories/aws/config/AwsConfig.java
+++ PhotoMemoriesAws/src/main/java/com/photomemories/aws/config/AwsConfig.java
@@ -17,7 +17,7 @@ public class AwsConfig {
 
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials("AKIAWFFGT4GVHIR6YE7N", "9V9HNDWJDFEorOGirwvz0zGSTrhdpF4cb2JgtOyG");
+        AWSCredentials awsCredentials = new BasicAWSCredentials("", "");
 
         return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).withRegion(Regions.US_EAST_1).build();
     }
